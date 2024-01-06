f3df74cf027e62a34cb117160c55c5eb63830af1
marotella <michelle.a.rotella@gmail.com>
2024-01-06 16:13:26 -0500

    add movie through DOM manipulation:

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+
+//Variables
+const form = document.querySelector("form")
+//Callbacks
+const addMovie = (evt) =>{
+    evt.preventDefault()
+    let inputField = document.querySelector("input");
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle)
+    let movieList = document.querySelector("ul");
+    movieList.appendChild(movie)
+    inputField.value = ""
+}
+
+//Event listeners
+form.addEventListener("submit", addMovie)
