64c27849d3838ee7406c72f0a50dd59695c1fa00
marotella <michelle.a.rotella@gmail.com>
2024-01-06 16:24:59 -0500

    Step 3 delete with DOM

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 
 //Variables
 const form = document.querySelector("form")
+let movieList = document.querySelector("ul");
+
 //Callbacks
 const addMovie = (evt) =>{
     evt.preventDefault()
     let inputField = document.querySelector("input");
     let movie = document.createElement("li");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", deleteMovie)
     let movieTitle = document.createElement("span");
     movieTitle.textContent = inputField.value;
-    movie.appendChild(movieTitle)
-    let movieList = document.querySelector("ul");
-    movieList.appendChild(movie)
-    inputField.value = ""
+    movie.appendChild(movieTitle);
+    movie.appendChild(deleteBtn);
+    movieList.appendChild(movie);
+    inputField.value = "";
+}
+
+const deleteMovie = (evt) => {
+    evt.target.parentNode.remove();
 }
 
 //Event listeners
-form.addEventListener("submit", addMovie)
+form.addEventListener("submit", addMovie);
